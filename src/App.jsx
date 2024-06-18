0c6f65683022d3a7fe37e292cb60f5ec86747a0e
ddas4548 <ddas4548@gmail.com>
2024-06-18 12:19:58 +0530

    besic UI is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 
 import './App.css'
+import Footer from './Footer'
 import Header from './Header'
 import Sidebar from './Sidebar'
+import MovieList from './cine/MovieList'
 
 function App() {
 
@@ -12,8 +14,10 @@ function App() {
     <main>
 		<div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
       <Sidebar/>
+      <MovieList/>
     </div>
     </main>
+    <Footer/>
     </>
   )
 }
